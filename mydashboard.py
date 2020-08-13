38693af77e5e2d1768a3f64a6336a7f1b15f816d
wfng <ngwf@yoozoo.com>
2020-08-13 16:12:28 +0800

    Updated based on proper naming convention

--- mydashboard.py
+++ mydashboard.py
@@ -6,40 +6,40 @@ import json
 
 app = Flask(__name__)
 
-def convert_info_from_locale(input):
+def get_stats(input):
     return locale.format_string('%d', input)
 
-def convert_currency_from_locale(input):
+def get_currencies(input):
     return locale.currency(input, international=True)
 
-def get_data_from_api():
-    global info_dict
-    global currency_dict
+def fetch_data():
+    global stats
+    global currencies
     global last_updated_time
 
-    info = {"message": 22450, "view": 578902, "share": 442, "user": 1824}
+    stat = {"message": 22450, "view": 578902, "share": 442, "user": 1824}
     currency = {"germany": 32.7, "united_kingdom": 16.5, "russia": 14.3, "spain": 10.8, "india": 7.6, "france": 4.9}
 
-    info_dict = {k: convert_info_from_locale(v) for k, v in info.items()}
-    currency_dict = {k: convert_currency_from_locale(v) for k, v in currency.items()}
+    stats = {k: get_stats(v) for k, v in stat.items()}
+    currencies = {k: get_currencies(v) for k, v in currency.items()}
 
     last_updated_time = datetime.datetime.now().strftime(date_format)
 
 
 @app.route('/dashboard/<language>')
 def dashboard(language):
-    if(language not in language_dict):
+    if(language not in languages):
         language = app_language
 
-    return render_template('index.html', **language_dict[language], info = info_dict, currency = currency_dict, update_time = last_updated_time)
+    return render_template('index.html', **languages[language], stats = stats, currencies = currencies, update_time = last_updated_time)
 
 if __name__ == '__main__':
     app_language = 'en_SG'
     locale.setlocale(locale.LC_ALL, app_language)
 
-    language_dict = {}
-    info_dict = {}
-    currency_dict = {}
+    languages = {}
+    stats = {}
+    currencies = {}
     date_format = "%d %b %Y %H:%M:%S %Z"
     last_updated_time = ""
 
@@ -48,9 +48,8 @@ if __name__ == '__main__':
         filename = lang.split('\\')
         lang_code = filename[1].split('.')[0]
 
-        with open(lang, 'r', encoding='utf8') as f:
-            json_string = "".join(f.readlines())
-            language_dict[lang_code] = json.loads(json_string)
+        with open(lang, 'r', encoding='utf8') as file:
+            languages[lang_code] = json.loads(file.read())
 
-    get_data_from_api()
+    fetch_data()
     app.run('0.0.0.0', port=5000)
